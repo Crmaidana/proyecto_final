6d54d95fa4e6b9af689b2f97f611147e7244154b
Crmaidana <claumai777@gmail.com>
2024-10-15 14:49:57 -0300

    user terminado

--- Infonomics/apps/blog_auth/urls.py
+++ Infonomics/apps/blog_auth/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path # type: ignore
 from django.contrib.auth.views import LoginView, LogoutView # type: ignore
-from .views import RegistrarseView
+from .views import RegistrarseView, EditarPerfil 
 
 
 app_name = 'apps.blog_auth'
@@ -9,5 +9,6 @@ app_name = 'apps.blog_auth'
 urlpatterns = [
     path("registrarse/", RegistrarseView.as_view(), name = 'registrarse'),
     path("iniciar_sesion/", LoginView.as_view(template_name='users/iniciar_sesion.html'), name = 'iniciar_sesion'),
-    path("cerrar_sesion/", LogoutView.as_view(), name = 'cerrar_sesion')
+    path("cerrar_sesion/", LogoutView.as_view(), name = 'cerrar_sesion'),
+    path("editar_perfil/<int:pk>", EditarPerfil.as_view(), name='editar_perfil') 
 ]
